b4d86ed66f7613a3f7f3c2a2bc2ef0693eea6f54
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-06-06 13:51:01 -0600

    Rat is born

---
+++ src/main.C
@@ -0,0 +1,27 @@
+#include "RatApp.h"
+#include "MooseInit.h"
+#include "Moose.h"
+#include "MooseApp.h"
+#include "AppFactory.h"
+
+// Create a performance log
+PerfLog Moose::perf_log("Rat");
+
+// Begin the main program.
+int
+main(int argc, char * argv[])
+{
+  // Initialize MPI, solvers and MOOSE
+  MooseInit init(argc, argv);
+
+  // Register this application's MooseApp and any it depends on
+  RatApp::registerApps();
+
+  // Create an instance of the application and store it in a smart pointer for easy cleanup
+  std::shared_ptr<MooseApp> app = AppFactory::createAppShared("RatApp", argc, argv);
+
+  // Execute the application
+  app->run();
+
+  return 0;
+}
